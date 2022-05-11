572cd58748afc7a7525100b54b468606d2d84e7d
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-05-11 20:05:43 +0100

    missing values method created using interpolation method and dropping rows if interpolation fails

--- EDA.py
+++ EDA.py
@@ -1,3 +1,5 @@
+import logging
+import pandas as pd
 class Preprocessing:
     def __init__(self, dataset) -> None:
         self.data = dataset
@@ -5,4 +7,31 @@ class Preprocessing:
         
     def check_missing_value(self):
         'purpose: checks for missing value in dataset'
+        missing_value_count = self.data.isnull().sum().sum()  # chaining methods together
+        if missing_value_count > 0:
+            logging.warning(f'{missing_value_count} missing values found. Using interpolation method to fill missing values')
+            self.interpolated_data = self.data.interpolate(method ='linear', limit_direction ='forward')
+
+            
+            #More Rigid Process
+            #check if misssing value is still present in interpolated data
+            missing_value_count = self.interpolated_data.isnull().sum().sum()
+            if missing_value_count > 0:
+                logging.warning(f'{missing_value_count} missing values found after using initial interpolation method. Dropping rows of missing values')
+                self.interpolated_data = self.interpolated_data.dropna()
+            
+            logging.warning(f'Missing Value problem solved..Data is clean and ready to use')
+            return self.interpolated_data
+        else:
+            logging.warning(f'No Missing Value was found data is clean and ready to use')
         
+    def descriptives(self, interpolated_data):
+        'purpose:finds the descriptives of the datasets'
+        logging.info(f'Showing descriptive statistics of the dataset')
+        pd.concat([interpolated_data.describe().T,
+                      interpolated_data.median().rename('median'),
+                      interpolated_data.skew().rename('skew'),
+                      interpolated_data.kurt().rename('kurt'),
+                     ], axis=1).T
+
+
